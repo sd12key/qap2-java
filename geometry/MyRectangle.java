0fa0be93275fa2f0efa5ca15dec86cdbd3130593
Eugene Kepich <166713967+sd12key@users.noreply.github.com>
2025-02-09 18:11:10 -0330

    Modifications to MyRectangle class: added logic for setters to verify that coordinates make sense (return false if not), added setters to change both rectangle point to a completely new ones. Added methonds to check if rectangle contains given point, and method to translate the rectangle by (dx,dy)

--- geometry/MyRectangle.java
+++ geometry/MyRectangle.java
@@ -134,95 +134,203 @@ public class MyRectangle {
         return xy;
     }
 
-    // ===================
+    // ==============================================================
     // setters for topLeft
-    // ===================
-    public void setTopLeft(MyPoint topLeft) {
-        this.topLeft = new MyPoint(topLeft);
+    // ==============================================================
+    // they return boolean True if the point is set, otherwise False. 
+    // check is done if topLeft coordinates make sense, that is,
+    // if topLeft is really top left corner of the rectangle
+    // ==============================================================
+    public boolean setTopLeft(MyPoint topLeft) {
+        if  ((topLeft.getX() <= this.bottomRight.getX()) 
+            && (topLeft.getY() >= this.bottomRight.getY())) {
+            this.topLeft = new MyPoint(topLeft);
+            return true;
+        }
+        return false;
     }
 
-    public void setTopLeftX(int x) {
-        this.topLeft.setX(x);
+    public boolean setTopLeftXY(int x, int y) {
+        if ((x <= this.bottomRight.getX()) && (y >= this.bottomRight.getY())) {
+            this.topLeft.setXY(x, y);
+            return true;
+        }
+        return false;
     }
 
-    public void setTopLeftY(int y) {
-        this.topLeft.setY(y);
+    public boolean setTopLeftX(int x) {
+        if (x <= this.bottomRight.getX()) {
+            this.topLeft.setX(x);
+            return true;
+        }
+        return false;
     }
 
-    public void setTopLeftXY(int x, int y) {
-        this.topLeft.setXY(x, y);
+    public boolean setTopLeftY(int y) {
+        if (y >= this.bottomRight.getY()) {
+            this.topLeft.setY(y);
+            return true;
+        }
+        return false;
     }
 
-    // =======================
+    // ==============================================================
     // setters for bottomRight
-    // =======================
-    public void setBottomRight(MyPoint bottomRight) {
-        this.bottomRight = new MyPoint(bottomRight);
+    // ==============================================================
+    // they return boolean True if the point is set, otherwise False. 
+    // check is done if bottomRight coordinates make sense, that is,
+    // if bottomRight is really bottom right corner of the rectangle
+    // ==============================================================
+    public boolean setBottomRight(MyPoint bottomRight) {
+        if  ((bottomRight.getX() >= this.topLeft.getX()) 
+            && (bottomRight.getY() <= this.topLeft.getY())) {
+            this.bottomRight = new MyPoint(bottomRight);
+            return true;
+        }
+        return false;
     }
 
-    public void setBottomRightX(int x) {
-        this.bottomRight.setX(x);
+    public boolean setBottomRightXY(int x, int y) {
+        if (x >= this.topLeft.getX() && y <= this.topLeft.getY()) {
+            this.bottomRight.setXY(x, y);
+            return true;
+        }
+        return false;
     }
 
-    public void setBottomRightY(int y) {
-        this.bottomRight.setY(y);
+    public boolean setBottomRightX(int x) {
+        if (x >= this.topLeft.getX()) {
+            this.bottomRight.setX(x);
+            return true;
+        }
+        return false;
     }
 
-    public void setBottomRightXY(int x, int y) {
-        this.bottomRight.setXY(x, y);
+    public boolean setBottomRightY(int y) {
+        if (y <= this.topLeft.getY()) {
+            this.bottomRight.setY(y);
+            return true;
+        }
+        return false;
     }
 
-    // ================================
+    // ==============================================================
     // setters for bottomLeft
-    // ================================
-    // since rectangle is defined by 
-    // topLeft and bottomRight, 
-    // we can't set bottomLeft directly
-    // but we can modify corresponding 
-    // coordinates of 
-    // topLeft and bottomRight
-    // ================================
-    public void setBottomLeft(MyPoint bottomLeft) {
-        this.topLeft.setX(bottomLeft.getX());
-        this.bottomRight.setY(bottomLeft.getY());
+    // ==============================================================
+    // since rectangle is defined by topLeft and bottomRight, 
+    // we can't set bottomLeft directly but we can modify
+    // corresponding coordinates of topLeft and bottomRight
+    // ==============================================================
+    // they return boolean True if the point is set, otherwise False. 
+    // check is done if bottomLeft coordinates make sense, that is,
+    // if bottomLeft is really bottom left corner of the rectangle
+    // ==============================================================
+    public boolean setBottomLeft(MyPoint bottomLeft) {
+        if  ((bottomLeft.getX() <= this.bottomRight.getX()) 
+            && (bottomLeft.getY() <= this.topLeft.getY())) {
+            this.topLeft.setX(bottomLeft.getX());
+            this.bottomRight.setY(bottomLeft.getY());
+            return true;
+        }
+        return false;    
     }
 
-    public void setBottomLeftX(int x) {
-        this.topLeft.setX(x);
+    public boolean setBottomLeftXY(int x, int y) {
+        if (x <= this.bottomRight.getX() && y <= this.topLeft.getY()) {
+            this.topLeft.setX(x);
+            this.bottomRight.setY(y);
+            return true;
+        }
+        return false;
     }
 
-    public void setBottomLeftY(int y) {
-        this.bottomRight.setY(y);
+    public boolean setBottomLeftX(int x) {
+        if (x <= this.bottomRight.getX()) {
+            this.topLeft.setX(x);
+            return true;
+        }
+        return false;
     }
 
-    public void setBottomLeftXY(int x, int y) {
-        this.topLeft.setX(x);
-        this.bottomRight.setY(y);
+    public boolean setBottomLeftY(int y) {
+        if (y <= this.topLeft.getY()) {
+            this.bottomRight.setY(y);
+            return true;
+        }
+        return false;
     }
 
-    // ========================
+    // ==============================================================
     // setters for topRight
-    // ========================
-    // same goes for topRight
-    // we modify coordinates of
-    // topLeft and bottomRight
-    // ========================
-    public void setTopRight(MyPoint topRight) {
-        this.bottomRight.setX(topRight.getX());
-        this.topLeft.setY(topRight.getY());
+    // ==============================================================
+    // same topRight: modify coordinates of topLeft and bottomRight
+    // ==============================================================
+    // they return boolean True if the point is set, otherwise False. 
+    // check is done if topRight coordinates make sense, that is,
+    // if topRight is really top right corner of the rectangle
+    // ==============================================================
+    public boolean setTopRight(MyPoint topRight) {
+        if  ((topRight.getX() >= this.topLeft.getX()) 
+            && (topRight.getY() >= this.bottomRight.getY())) {
+            this.bottomRight.setX(topRight.getX());
+            this.topLeft.setY(topRight.getY());
+            return true;
+        }
+        return false;
     }
 
-    public void setTopRightX(int x) {
-        this.bottomRight.setX(x);
+    public boolean setTopRightXY(int x, int y) {
+        if (x >= this.topLeft.getX() && y >= this.bottomRight.getY()) {
+            this.bottomRight.setX(x);
+            this.topLeft.setY(y);
+            return true;
+        }
+        return false;
     }
 
-    public void setTopRightY(int y) {
-        this.topLeft.setY(y);
+    public boolean setTopRightX(int x) {
+        if (x >= this.topLeft.getX()) {
+            this.bottomRight.setX(x);
+            return true;
+        }
+        return false;
     }
 
-    public void setTopRightXY(int x, int y) {
-        this.bottomRight.setX(x);
-        this.topLeft.setY(y);
+    public boolean setTopRightY(int y) {
+        if (y >= this.bottomRight.getY()) {
+            this.topLeft.setY(y);
+            return true;
+        }
+        return false;
+    }
+
+    // ====================================
+    // setter for changing rectangle points
+    // ====================================
+    public void setAllRectanglePoints(MyPoint p1, MyPoint p2) {
+        // calculate the correct topLeft and bottomRight points 
+        // based on the coordinates of p1 and p2
+        int topLeftX = Math.min(p1.getX(), p2.getX());
+        int topLeftY = Math.max(p1.getY(), p2.getY());
+        int bottomRightX = Math.max(p1.getX(), p2.getX());
+        int bottomRightY = Math.min(p1.getY(), p2.getY());
+    
+        // set new the rectangle points
+        this.topLeft = new MyPoint(topLeftX, topLeftY);
+        this.bottomRight = new MyPoint(bottomRightX, bottomRightY);
+    }
+
+    public void setAllRectanglePoints(int x1, int y1, int x2, int y2) {
+        // calculate the correct topLeft and bottomRight points 
+        // based on the coordinates x1, y1 and x2, y2
+        int topLeftX = Math.min(x1, x2);
+        int topLeftY = Math.max(y1, y2);
+        int bottomRightX = Math.max(x1, x2);
+        int bottomRightY = Math.min(y1, y2);
+    
+        // set new the rectangle points
+        this.topLeft = new MyPoint(topLeftX, topLeftY);
+        this.bottomRight = new MyPoint(bottomRightX, bottomRightY);
     }
 
     // =============
@@ -262,12 +370,12 @@ public class MyRectangle {
 
     // get the center x-coordinate
     public double getCenterX() {
-        return (((double)this.topLeft.getX() + (double)this.bottomRight.getX()) / 2.0);
+        return ((this.topLeft.getX() + this.bottomRight.getX()) / 2.0);
     }
 
     // get the center y-coordinate
     public double getCenterY() {
-        return (((double)this.topLeft.getY() + (double)this.bottomRight.getY()) / 2.0);
+        return ((this.topLeft.getY() + this.bottomRight.getY()) / 2.0);
     }
 
     // get the center coordinates as [x,y]
@@ -281,6 +389,24 @@ public class MyRectangle {
     // length of the diagonal
     public double getDiagonalLength() {
         return Math.sqrt(this.getWidth() * this.getWidth() + this.getHeight() * this.getHeight());
+    }
+
+    // check if a point is inside the rectangle (myPoint)
+    public boolean contains(MyPoint p) {
+        return ((p.getX() >= this.topLeft.getX()) && (p.getX() <= this.bottomRight.getX()) &&
+               (p.getY() <= this.topLeft.getY()) && (p.getY() >= this.bottomRight.getY()));
+    }
+
+    // check if a point is inside the rectangle (x,y)
+    public boolean contains(int x, int y) {
+        return ((x >= this.topLeft.getX()) && (x <= this.bottomRight.getX()) &&
+               (y <= this.topLeft.getY()) && (y >= this.bottomRight.getY()));
+    }    
+
+    // translate the rectangle by dx and dy
+    public void translate(int dx, int dy) {
+        this.topLeft.setXY(this.topLeft.getX() + dx, this.topLeft.getY() + dy);
+        this.bottomRight.setXY(this.bottomRight.getX() + dx, this.bottomRight.getY() + dy);
     }
 
     // toString method
